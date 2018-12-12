4a5c454b0a23935aef0e5bd87b10747cd6662c6d
Rodrigo Gurgel <rodrigohenrique.gurgel@outlook.com>
2018-12-11 22:22:42 -0200

    alteração no funcionamento e retorno da classe principal

--- index.js
+++ index.js
@@ -6,30 +6,28 @@ function ValidationContract() {
     errors = [];
 }
 
-ValidationContract.prototype.isRequired = (value, message) => {
-    if (!value || value.length <= 0)
-        errors.push({ message: message });
+ValidationContract.prototype.valide = (name, value, validations = []) => {
+    let error = [];
+    validations.map((validation) => {
+        var result = validation(value, name);
+        if (result) {
+            error.push(result);
+        }
+    });
+    errors.push({ name, error, valide: error.length == 0 });
 }
 
-ValidationContract.prototype.hasMinLen = (value, min, message) => {
-    if (!value || value.length < min)
-        errors.push({ message: message });
-}
+ValidationContract.prototype.isRequired = (message = "The '{0}' field is required") => (value, name) => (!value || value.length <= 0) ? message.replace("{0}", name) : "";
 
-ValidationContract.prototype.hasMaxLen = (value, max, message) => {
-    if (!value || value.length > max)
-        errors.push({ message: message });
-}
+ValidationContract.prototype.hasMinLen = (min, message) => (value, name) => (!value || value.length < min) ? message : "";
 
-ValidationContract.prototype.isFixedLen = (value, len, message) => {
-    if (value.length != len)
-        errors.push({ message: message });
-}
+ValidationContract.prototype.hasMaxLen = (max, message) => (value, name) => (!value || value.length > max) ? message : "";
 
-ValidationContract.prototype.isEmail = (value, message) => {
+ValidationContract.prototype.isFixedLen = (len, message) => (value, name) => (value.length != len) ? message : "";
+
+ValidationContract.prototype.isEmail = (message) => (value, name) => {
     var reg = new RegExp(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/);
-    if (!reg.test(value))
-        errors.push({ message: message });
+    return !reg.test(value) ? message : "";
 }
 
 ValidationContract.prototype.errors = () => {
@@ -38,10 +36,6 @@ ValidationContract.prototype.errors = () => {
 
 ValidationContract.prototype.clear = () => {
     errors = [];
-}
-
-ValidationContract.prototype.isValid = () => {
-    return errors.length == 0;
 }
 
 module.exports = ValidationContract;
